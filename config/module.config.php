1faba39f256f53e21ba766815ba458f21e17a853
Jan Esser <esserj@gmail.com>
2013-11-14 20:16:41 +0100

    added 403 template as zfcRbac's template expects an array

--- config/module.config.php
+++ config/module.config.php
@@ -32,5 +32,10 @@ return array(
                 )
             )
         )
-    )
+    ),
+    'view_manager' => array(
+        'template_map' => array(
+            'error/403' => __DIR__ . '/../view/error/403.phtml',
+        )
+    ),
 );
