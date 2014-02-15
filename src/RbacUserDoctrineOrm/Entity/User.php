ce799bfe63bd67a499a2918b08b3400403ac35cc
jmfiaschi <jm.fiaschi@gmail.com>
2014-02-15 12:58:29 +0100

    * Modification of configuration * Modification of entities for rbac 2.*

--- src/RbacUserDoctrineOrm/Entity/User.php
+++ src/RbacUserDoctrineOrm/Entity/User.php
@@ -15,34 +15,34 @@
 
 namespace RbacUserDoctrineOrm\Entity;
 
-
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\PersistentCollection;
 use ZfcRbac\Identity\IdentityInterface;
-use ZfcUser\Entity\User as ZfcUserEntity;
+use ZfcUserDoctrineORM\Entity\User as ZfcUserDoctrineORMUser;
 
-class User extends ZfcUserEntity implements IdentityInterface
+
+class User extends ZfcUserDoctrineORMUser implements IdentityInterface
 {
-
-    /**
-     * @var Role[]
-     */
-    protected $roles;
-
-    /**
-     * @return PersistentCollection|Role[]
-     */
-    public function getRoles()
-    {
-        return $this->roles;
-    }
-
-    /**
-     * @param PersistentCollection $roles
-     * @return self
-     */
-    public function setRoles(PersistentCollection $roles)
-    {
-        $this->roles = $roles;
-        return $this;
-    }
+	/**
+	 * @var Role[]
+	 */
+	protected $roles;
+	
+	/**
+	 * @return PersistentCollection|Role[]
+	 */
+	public function getRoles()
+	{
+		return $this->roles;
+	}
+	
+	/**
+	 * @param PersistentCollection $roles
+	 * @return self
+	 */
+	public function setRoles(PersistentCollection $roles)
+	{
+		$this->roles = $roles;
+		return $this;
+	}
 }
